7227863cf93bb756d1cc7a8f2b9f8b6b7e97d97e
王野 <18545455617@163.com>
2026-03-05 14:14:23 +0800

    1

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: 王野 18545455617@163.com
+ * @Date: 2026-03-04 16:35:26
+ * @LastEditors: 王野 18545455617@163.com
+ * @LastEditTime: 2026-03-04 17:01:37
+ * @FilePath: /store/nuxt.config.ts
+ * @Description: 前端nuxt配置文件
+ */
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: `2025-07-15`,
+  css: [`element-plus/dist/index.css`],
+  devServer: {
+    port: 3000 // 前端运行端口
+  },
+  devtools: { enabled: true },
+  modules: [`@element-plus/nuxt`],
+});
